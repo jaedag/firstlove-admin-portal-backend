de050b5ba1d8bbed2908f99a56db4a5e952cdf9c
John-Dag Addy <jaeadgy@gmail.com>
2020-07-27 11:51:43 +0000

    after tweaking project to work on jd laptop and the initial front end mockup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import {
-  typeDefs
-} from "./graphql-schema";
-import {
-  ApolloServer
-} from "apollo-server-express";
-import express from "express";
-import {
-  v1 as neo4j
-} from "neo4j-driver";
-import {
-  makeAugmentedSchema
-} from "neo4j-graphql-js";
-import dotenv from "dotenv";
+import { typeDefs } from './graphql-schema';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { v1 as neo4j } from 'neo4j-driver';
+import { makeAugmentedSchema } from 'neo4j-graphql-js';
+import dotenv from 'dotenv';
 
 // set environment variables from ../.env
 dotenv.config();
@@ -27,7 +19,7 @@ const app = express();
  */
 
 const schema = makeAugmentedSchema({
-  typeDefs
+	typeDefs
 });
 
 /*
@@ -36,11 +28,11 @@ const schema = makeAugmentedSchema({
  * with fallback to defaults
  */
 const driver = neo4j.driver(
-  process.env.NEO4J_URI || "bolt://localhost:7687",
-  neo4j.auth.basic(
-    process.env.NEO4J_USER || "terryb",
-    process.env.NEO4J_PASSWORD || "letmein"
-  )
+	process.env.NEO4J_URI || 'bolt://localhost:11002',
+	neo4j.auth.basic(process.env.NEO4J_USER || 'jd_addy', process.env.NEO4J_PASSWORD || 'pneumatik0s'),
+	{
+		encrypted: 'ENCRYPTION_OFF'
+	}
 );
 
 /*
@@ -50,30 +42,33 @@ const driver = neo4j.driver(
  * generated resolvers to connect to the database.
  */
 const server = new ApolloServer({
-  context: {
-    driver
-  },
-  schema: schema,
-  introspection: true,
-  playground: true
+	context: {
+		driver
+	},
+	schema: schema,
+	introspection: true,
+	playground: true
 });
 
 // Specify port and path for GraphQL endpoint
 const port = process.env.GRAPHQL_LISTEN_PORT || 4001;
-const path = "/graphql";
+const path = '/graphql';
 
 /*
  * Optionally, apply Express middleware for authentication, etc
  * This also also allows us to specify a path for the GraphQL endpoint
  */
 server.applyMiddleware({
-  app,
-  path
+	app,
+	path
 });
 
-app.listen({
-  port,
-  path
-}, () => {
-  console.log(`GraphQL server ready at http://localhost:${port}${path}`);
-});
+app.listen(
+	{
+		port,
+		path
+	},
+	() => {
+		console.log(`GraphQL server ready at http://localhost:${port}${path}`);
+	}
+);
