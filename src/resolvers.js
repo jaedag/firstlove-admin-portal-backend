adbec6d46df091e0171ea6e23d8d20615d1dcb23
John-Dag Addy <jaeadgy@gmail.com>
2020-08-31 07:51:37 +0000

    full backend for use in project-work

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,22 +1,3 @@
-//import { neo4jgraphql } from 'neo4j-graphql-js';
-//
-//export const resolvers = {
-//	Mutation: {
-//		async addMember(parent, args, context, info) {
-//			const member = new Member({
-//				firstName: args.firstName,
-//				lastName: args.lastName,
-//				email: args.email,
-//				phoneNumber: args.phoneNumber,
-//				whatsAppNumber: args.whatsAppNumber,
-//				gender: args.gender
-//			});
-//			await member.save();
-//			return member;
-//		}
-//	}
-//};
-
 const resolvers = {
 	Mutation: {
 		/* NEW USER */
@@ -25,6 +6,7 @@ const resolvers = {
 			const findUser = await ctx.driver.session().run(`MATCH (m:Member {email: "${params.email}"}) return m`);
 			if (findUser.records.length > 0) {
 				throw new ApolloError('This user already exists', 200, [ 'user_already_exists' ]);
+				return 'This user already exists';
 			}
 			return neo4jgraphql(obj, params, ctx, resolveInfo, true);
 		}
